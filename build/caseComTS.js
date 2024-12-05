7b5644b31b26ce138970d471295fd1dace4bea9d
Marcos Gabriel <marcosindev@gmail.com>
2024-12-05 18:02:48 -0300

    Config e teste com TS

---
+++ build/caseComTS.js
@@ -0,0 +1,9 @@
+"use strict";
+function ligarPara(heroi) {
+    return "Ligando para: " + heroi.telefone;
+}
+ligarPara({
+    nome: "Marquim",
+    vulgo: "Capitão América",
+    telefone: "85 111222333"
+});
